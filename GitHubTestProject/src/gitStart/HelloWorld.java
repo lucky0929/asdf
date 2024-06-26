1c0db728fb4500cf952e6000f4b7436ed9618303
lucky0929 <fhrrnl0929@gmail.com>
2024-06-27 00:48:21 +0900

    ㅋㅌㅊ

--- GitHubTestProject/src/gitStart/HelloWorld.java
+++ GitHubTestProject/src/gitStart/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 		System.out.println("Git Branch Test");
 		System.out.println("이거 왜 안 되나요?? ?");    
 		System.out.println("이건 되나..");
-		System.out.println("잘수있나?");
+		System.out.println("잘수있나 ?");
 	}
 
 }
